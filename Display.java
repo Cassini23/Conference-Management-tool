e1bc42cfaca796893de73319e5cf2c8c57fe5a0a
Cassini23 <nbalach1@binghamton.edu>
2015-01-28 21:27:20 -0500

    Display

---
+++ Display.java
@@ -0,0 +1,24 @@
+package designConf.util;
+/**
+ * Display class displays results
+ */
+
+public class Display {
+	
+	
+	 /**
+     * Display constructor
+    
+     */
+	public Display() {}
+	
+	/**
+     *  display method displays results
+     */
+	public String display(ResultInterface result){
+
+		 return result.toString();
+	}
+
+
+}
