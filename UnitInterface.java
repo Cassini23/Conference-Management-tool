fced305736ce9e41cb9cf44301faa42f6c22d522
Cassini23 <nbalach1@binghamton.edu>
2015-01-28 21:34:12 -0500

    UnitInterface

---
+++ UnitInterface.java
@@ -0,0 +1,12 @@
+package designConf.util;
+
+/**
+ * UnitInterface includes static and final variables for units of display
+*/
+public interface UnitInterface {
+	
+	static final String currency = "dollars";
+	static final String timeMeasure = "minutes";
+	static final String CO2Unit = "tonnes";
+
+}
