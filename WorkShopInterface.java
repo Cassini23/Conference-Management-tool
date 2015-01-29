85fa5473d62cc5b5f5de571f076cae18100a318b
Cassini23 <nbalach1@binghamton.edu>
2015-01-28 21:30:44 -0500

    WorkShopInterface
    Interface

---
+++ WorkShopInterface.java
@@ -0,0 +1,18 @@
+package designConf.util;
+
+import designConf.util.ActivityBuilder;
+
+
+/**
+ * Workshop interface
+ */
+
+public interface WorkShopInterface {
+	/**
+	 * constuct method to be overriden by implementations
+	 * @param builder : activityBuilder arguement
+	 */
+	public void construct(ActivityBuilder builder);
+
+
+}
